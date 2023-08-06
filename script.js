ac6e6a7757e0850e06411e248ea2c162a60898d2
sarperxx <43006015+sarperxx@users.noreply.github.com>
2023-08-06 14:31:55 +0300

    added equal function and other functions

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let buttons = [...document.querySelectorAll(".btn")];
+let numberButtons = document.querySelectorAll(".btn");
+let operatorButtons = document.querySelectorAll(".operator");
+let currentOperand = document.getElementById("current-operand");
+let previousOperand = document.getElementById("previous-operand");
+let equalsKey = document.getElementById("equals-key");
 // gets input from the user
 const calcInput = document.getElementById("result");
 // addition
@@ -21,45 +27,66 @@ function operate(operator, n1, n2) {
   switch (operator) {
     case "+":
       console.log("passing through switch + ");
-      alert(addition(n1, n2));
-      break;
+      return addition(n1, n2);
     case "-":
       console.log("passing through switch - ");
-      alert(substraction(n1, n2));
-      break;
+      return substraction(n1, n2);
     case "*":
       console.log("passing through switch * ");
-      alert(multiply(n1, n2));
-      break;
+      return multiply(n1, n2);
     case "/":
       console.log("passing through switch * ");
-      alert(division(n1, n2));
-      break;
+      return division(n1, n2);
   }
 }
-function dis(val) {
-  calcInput.value += val;
-}
+currentOperand.textContent = " ";
+previousOperand.textContent = " ";
+// variables
+let storedNumber = "";
+let clickedOperator = "";
+let firstNumber = "";
+let secondNumber = "";
+let result = "";
+currentOperand.textContent = 0;
 
 function clr() {
-  calcInput.value = "";
+  currentOperand.textContent = "";
+  previousOperand.textContent = "";
 }
-if (calcInput.innerText.includes("=")) {
-  if (calcInput.innerText.includes("+")) {
-    const splitStuff = calcInput.innerText.split("+");
-    clr();
-    calcInput.value = operate("+", int(splitStuff[0]), int(splitStuff[1]));
-  } else if (calcInput.innerText.includes("-")) {
-    const splitStuff = calcInput.innerText.split("-");
-    clr();
-    calcInput.value = operate("-", int(splitStuff[0]), int(splitStuff[1]));
-  } else if (calcInput.innerText.includes("*")) {
-    const splitStuff = calcInput.innerText.split("*");
-    clr();
-    calcInput.value = operate("*", int(splitStuff[0]), int(splitStuff[1]));
-  } else if (calcInput.innerText.includes("/")) {
-    const splitStuff = calcInput.innerText.split("/");
-    clr();
-    calcInput.value = operate("/", int(splitStuff[0]), int(splitStuff[1]));
-  }
-}
+// pop out numbers
+numberButtons.forEach((number) => {
+  number.addEventListener("click", function () {
+    storedNumber += number.value;
+    currentOperand.textContent = storedNumber;
+  });
+});
+
+operatorButtons.forEach((operator) => {
+  operator.addEventListener("click", function () {
+    // save the first number
+    firstNumber = storedNumber;
+
+    // get the operator that was clicked
+    clickedOperator = operator.value;
+    previousOperand.textContent = storedNumber + clickedOperator;
+    storedNumber = "";
+
+    console.log(clickedOperator);
+  });
+});
+
+equalsKey.addEventListener("click", function () {
+  console.log("FirstNumber" + firstNumber + "Stored" + storedNumber);
+
+  // operates the result
+  result = operate(
+    clickedOperator,
+    parseInt(firstNumber),
+    parseInt(storedNumber)
+  );
+
+  currentOperand.textContent = result;
+  previousOperand.textContent =
+    firstNumber + " " + clickedOperator + " " + storedNumber;
+  storedNumber = result;
+});
